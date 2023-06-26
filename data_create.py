fb532531e70e663e27078c0a9f327ee56c2df8d7
Alexeeva Ekaterina <kat26031995@mail.ru>
2023-06-26 19:06:49 +0300

    Создание справочника

---
+++ data_create.py
@@ -0,0 +1,20 @@
+def name_data():
+    name = input('Введите Ваше имя: ')
+    print('Welcome, фрэндс. Ты здесь, потому что помогаешь Кате разобраться')
+    return name
+
+
+def surname_data():
+    surname = input('Введите Вашу фамилию: ')
+    return surname
+
+
+def phone_data():
+    # import re
+    phone = input('Введите Ваш телефон: ')
+    return phone
+
+
+def address_data():
+    address = input('Введите Ваш адрес: ')
+    return address
